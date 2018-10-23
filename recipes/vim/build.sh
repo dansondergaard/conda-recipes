dbbf60f743174c2abb76074dce10b1357d7fba50
Dan Søndergaard <das@birc.au.dk>
2018-10-23 11:20:32 +0200

    Add recipe for vim

---
+++ recipes/vim/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+./configure --prefix=$PREFIX    \
+            --without-x         \
+            --without-gnome     \
+            --without-tclsh     \
+            --without-local-dir \
+            --enable-gui=no     \
+            --enable-cscope     \
+            --enable-python3interp=dynamic
+
+make -j
+make install
